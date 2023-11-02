98580a11d484527984f9f6d9e76a5e8ec969a489
Luolan (Gloria) Li <gloriali@users.noreply.github.com>
2023-11-01 17:37:09 -0700

    Add tax column.

--- woo2sqaure.R
+++ woo2sqaure.R
@@ -8,7 +8,8 @@ woo1$Stock <- as.numeric(woo1$Stock)
 sales <- woo1 %>% filter(!is.na(woo1$Sale.price)) 
 rownames(sales) <- sales$SKU
 
-square <- data.frame(ItemName = woo1$SKU, SKU = woo1$SKU, Description = "", Category = gsub("\\ *\\|.*", "", woo1$Name), Price = woo1$Regular.price, PriceRichmond = woo1$Regular.price, PriceSurrey = woo1$Regular.price, NewQuantityRichmond = woo1$Stock, NewQuantitySurrey = woo1$Stock, Token = "", VariationName = "Regular", EnabledRichmond = "Y", CurrentQuantityRichmond = "", CurrentQuantitySurrey = "", StockAlertEnabledRichmond = "N", StockAlertCountRichmond = 0, EnabledSurrey = "Y", StockAlertEnabledSurrey = "N", StockAlertCountSurrey = 0)
+square <- data.frame(ItemName = woo1$SKU, SKU = woo1$SKU, Description = "", Category = gsub("\\ *\\|.*", "", woo1$Name), Price = woo1$Regular.price, PriceRichmond = woo1$Regular.price, PriceSurrey = woo1$Regular.price, NewQuantityRichmond = woo1$Stock, NewQuantitySurrey = woo1$Stock, Token = "", VariationName = "Regular", EnabledRichmond = "Y", CurrentQuantityRichmond = "", CurrentQuantitySurrey = "", StockAlertEnabledRichmond = "N", StockAlertCountRichmond = 0, EnabledSurrey = "Y", StockAlertEnabledSurrey = "N", StockAlertCountSurrey = 0, TaxPST = woo1$Tax.class)
 rownames(square) <- square$SKU
+square$TaxPST <- gsub("parent", "N", gsub("full", "Y", square$TaxPST))
 square[sales$SKU, "Price"] <- sales[sales$SKU, "Sale.price"]
-write.csv(square, file = paste0("../Square/square-upload-", Sys.Date(), ".csv"), row.names = F, na = "")
+write.csv(square, file = paste0("../Square/square-upload-", Sys.Date(), ".csv"), row.names = F, col.names = c(colnames(square)[1:ncol(square)-1], "Tax-PST(7%)"), na = "")
