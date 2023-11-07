806548712fbf9a7c6680c0d0ef01dcf1321b80c9
Luolan (Gloria) Li <gloriali@users.noreply.github.com>
2023-11-07 14:52:45 -0800

    Add POS sales to yotpo rewards program.

--- README.md
+++ README.md
@@ -4,3 +4,4 @@
 [requestStock.R](./requestStock.R): generate inventory request from Surrey to Richmond - run at request and email Shikshit.    
 [square2xoro.sh](./square2xoro.sh): prepare Square sales for xoro upload - run at request and email Garman.   
 [woo2square.R](./woo2square.R): sync square inventory with woo - update routinely.    
+[reward.R](./reward.R): add POS sales to yotpo rewards program system.    
---
+++ reward.R
@@ -0,0 +1,24 @@
+# add POS sales to yotpo rewards program system
+## --------------- Clover sales to yotpo -----------
+# customer info: Clover > Customers > Download
+# sales: Clover > Transactions > Payments > select date > Export
+library(dplyr)
+customer <- read.csv(paste0("../Clover/", list.files(path = "../Clover/", pattern = paste0("Customers-", format(Sys.Date(), "%Y%m%d")))), as.is = T) %>%
+  mutate(Name = paste0(First.Name, " ", Last.Name)) %>% 
+  filter(Name != " ", Email.Address != "")
+rownames(customer) <- customer$Name
+
+payments <- read.csv(paste0("../Clover/", list.files(path = "../Clover/", pattern = paste0("Payments-", format(Sys.Date(), "%Y%m%d")))), as.is = T) 
+payments$email <- customer[payments$Customer.Name, "Email.Address"]
+payments[is.na(payments$Refund.Amount), "Refund.Amount"] <- 0
+payments <- payments %>% filter(!is.na(email))
+
+point <- data.frame(Email = payments$email, Points = as.integer(payments$Amount - payments$Refund.Amount))
+
+## --------------- Square sales to yotpo --------------
+square_customer <- read.csv(paste0("../Square/", list.files(path = "../Square/", pattern = paste0("customers-", format(Sys.Date(), "%Y%m%d")))), as.is = T)
+rownames(square_customer) <- square_customer$Square.Customer.ID
+transactions <- read.csv(paste0("../Square/", list.files(path = "../Square/", pattern = paste0("transactions-.*", format(Sys.Date(), "%Y-%m-%d")))), as.is = T) %>%
+  filter(Customer.ID != "")
+point <- bind_rows(point, data.frame(Email = square_customer[transactions$Customer.ID, "Email.Address"], Points = as.integer(gsub("\\$", "", transactions$Total.Collected))))
+write.csv(point, file = paste0("../yotpo/", format(Sys.Date(), "%m%d%Y"), "-yotpo.csv"), row.names = F)
