a9383d5086732676a3d8d3f67c0a82c9d53af7ee
Luolan (Gloria) Li <gloriali@users.noreply.github.com>
2023-11-09 16:44:03 -0800

    use openxlsx for clover input and output.

--- README.md
+++ README.md
@@ -1,7 +1,6 @@
 # Jan and Jul system management   
-[clover.R](./clover.R): manage Clover system - price, barcode, stock etc - update routinely.   
+[clover.R](./clover.R): manage Clover system - price, barcode, stock etc - update routinely; generate inventory request from Surrey to Richmond - run at request and email Shikshit.    
 [customers.sh](./customers.sh): combine customers from Clover, Square, and Stripe.    
-[requestStock.R](./requestStock.R): generate inventory request from Surrey to Richmond - run at request and email Shikshit.    
 [square2xoro.sh](./square2xoro.sh): prepare Square sales for xoro upload - run at request and email Garman.   
-[woo2square.R](./woo2square.R): sync square inventory with woo - update routinely.    
-[reward.R](./reward.R): add POS sales to yotpo rewards program system.    
+[square.R](./square.R): sync square inventory with xoro, price with woo - update routinely.    
+[reward.R](./reward.R): add POS sales to yotpo rewards program system - update routinely.    
--- clover.R
+++ clover.R
@@ -1,31 +1,55 @@
 # -------- woo price for clover -------- 
-# download current Surrey stock: wc > Products > All Products > Export.
-# download clover inventory: Clover > Inventory > Items > Export.
+# download current woo price: wc > Products > All Products > Export.
+# download clover inventory: clover_item > Inventory > Items > Export.
 library(dplyr)
-woo <- read.csv("../woo/wc-product-export-31-10-2023-1698777245011.csv", as.is = T)
-woo1 <- woo %>% filter(!is.na(woo$Regular.price) & !duplicated(woo$SKU) & !(woo$SKU == "")) 
-rownames(woo1) <- woo1$SKU
-sales <- woo1 %>% filter(!is.na(woo1$Sale.price)) 
+library(openxlsx)
+woo <- read.csv(paste0("../woo/", list.files(path = "../woo/", pattern = paste0("wc-product-export-", sub("-0", "", sub("^0", "", format(Sys.Date(), "%d-%m-%Y")))))), as.is = T) %>% 
+  filter(!is.na(Regular.price) & !duplicated(SKU) & !(SKU == "")) 
+rownames(woo) <- woo$SKU
+sales <- woo %>% filter(!is.na(woo$Sale.price)) 
 rownames(sales) <- sales$SKU
 
-clover <- read.csv("../Clover/inventory20231031-items.csv", as.is = T)
-rownames(clover) <- clover$Name
-clover$Price <- woo1[clover$Name, "Regular.price"]
-clover[sales$SKU, "Price"] <- sales[sales$SKU, "Sale.price"]
-write.csv(clover, file = "../Clover/inventory20231031-items-upload.csv", row.names = F, na = "")
+clover <- loadWorkbook(paste0("../Clover/", list.files(path = "../Clover/", pattern = paste0("inventory", format(Sys.Date(), "%Y%m%d"), ".xlsx"))))
+clover_item <- readWorkbook(clover, "Items") %>%
+  mutate(Price = ifelse(Name %in% sales$SKU, sales[Name, "Sale.price"], woo[Name, "Regular.price"]))
+clover_item <- clover_item %>% rename_with(~ gsub("\\.", " ", colnames(clover_item)))
+writeData(clover, "Items", clover_item)
+saveWorkbook(clover, file = paste0("../Clover/inventory", format(Sys.Date(), "%Y%m%d"), "-upload.xlsx"))
 
-# -------- master file barcode for Clover -------------
-# master SKU file: OneDrive > TWK 2020 share
+# ---------------- request stock from Surrey warehouse --------------------
+# download current Surrey stock: wc > Products > All Products > Export; 
+# download current Richmond stock: clover_item > Inventory > Items > Export
 library(dplyr)
+library(openxlsx)
+request <- c("ICP", "IPC", "IPS", "ISB", "WJA", "WMR", "WMT", "WPF")
+xoro <- read.csv(paste0("../xoro/", list.files(path = "../xoro/", pattern = paste0("Item Inventory Snapshot_", format(Sys.Date(), "%m%d%Y"), ".csv"))), as.is = T) %>% filter(Store == "Warehouse - JJ")
+rownames(xoro) <- xoro$Item.
+
+clover <- loadWorkbook(paste0("../Clover/", list.files(path = "../Clover/", pattern = paste0("inventory", format(Sys.Date(), "%Y%m%d"), ".xlsx"))))
+clover_item <- readWorkbook(clover, "Items") %>% mutate(cat = gsub("-.*", "", Name))
+clover_item[is.na(clover_item$Quantity), "Quantity"] <- 0
+
+order <- data.frame(StoreCode = "WH-JJ", ItemNumber=(clover_item %>% filter(Quantity<1 & cat %in% request))$Name, Qty = 1, LocationName = "BIN", UnitCost = "", ReasonCode = "RWT", Memo = "Richmond Transfer to Miranda", UploadRule = "D", AdjAccntName = "", TxnDate = "", ItemIdentifierCode = "", ImportError = "")
+order <- order %>% filter(xoro[order$ItemNumber, "ATS"] > 10)
+write.csv(order, file = paste0("../Clover/order", format(Sys.Date(), "%m%d%Y"), ".csv"), row.names = F, na = "")
+
+# -------- master file barcode for clover_item -------------
+# master SKU file: OneDrive > TWK 2020 share
+# download clover inventory: clover_item > Inventory > Items > Export.
+library(dplyr)
+library(openxlsx)
 mastersku <- read.csv("../woo/1-MasterSKU-All-Product-2023-10-25.csv", skip = 3, header = T, as.is = T, colClasses = c(UPC.Active = "character"))
 rownames(mastersku) <- mastersku$MSKU
-clover <- read.csv("../Clover/inventory20231102-items.csv", as.is = T)
-clover$Product.Code <- mastersku[clover$SKU, "UPC.Active"]
-write.csv(clover, file = "../Clover/inventory20231102-items-upload.csv", row.names = F, na = "")
+
+clover <- loadWorkbook(paste0("../Clover/", list.files(path = "../Clover/", pattern = paste0("inventory", format(Sys.Date(), "%Y%m%d"), ".xlsx"))))
+clover_item <- readWorkbook(clover, "Items") %>% mutate(Product.Code = mastersku[SKU, "UPC.Active"])
+clover_item <- clover_item %>% rename_with(~ gsub("\\.", " ", colnames(clover_item)))
+writeData(clover, "Items", clover_item)
+saveWorkbook(clover, file = paste0("../Clover/inventory", format(Sys.Date(), "%Y%m%d"), "-upload.xlsx"))
 
 # -------- Combine Vancouver Baby left over with Richmond stock -------- 
 library(dplyr)
-vbaby <- read.csv("../Clover/inventory20231030-items.csv", as.is = T)
+vbaby <- read.csv("../clover_item/inventory20231030-items.csv", as.is = T)
 rownames(vbaby) <- vbaby$Name
 vbaby[is.na(vbaby$Quantity), "Quantity"] <- 0
 richmond <- read.csv("../Square/catalog-2023-10-28-1448.csv", as.is = T)
@@ -35,4 +59,4 @@ vbaby$Price <- as.numeric(richmond[vbaby$Name, "Price"])
 vbaby$Quantity <- vbaby$Quantity + richmond[vbaby$Name, "Current.Quantity.Jan...Jul.Richmond"]
 vbaby[vbaby$Quantity < 0, "Quantity"] <- 0
 vbaby[grep("NA", vbaby$Product.Code), "Product.Code"] <- ""
-write.csv(vbaby, file = "../Clover/inventory20231030-items-upload.csv", row.names = F, na = "")
+write.csv(vbaby, file = "../clover_item/inventory20231030-items-upload.csv", row.names = F, na = "")
