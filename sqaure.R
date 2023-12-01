a389ad7fcbce505b95c05e82aeff1635c80d7b10
Luolan (Gloria) Li <gloriali@users.noreply.github.com>
2023-12-01 11:48:20 -0800

    Update to website price.

--- sqaure.R
+++ sqaure.R
@@ -4,8 +4,8 @@ library(dplyr)
 # ------- match Square price to woo ------
 # input: woo > Products > All Products > Export > all
 woo <- read.csv(paste0("../woo/", list.files(path = "../woo/", pattern = paste0("wc-product-export-", sub("-0", "", sub("^0", "", format(Sys.Date(), "%d-%m-%Y")))))), as.is = T)
-woo1 <- woo %>% filter(!is.na(woo$Regular.price) & !duplicated(woo$SKU) & !(woo$SKU == "")) 
-sales <- woo1 %>% filter(!is.na(woo1$Sale.price)) 
+woo <- woo %>% filter(!is.na(woo$Regular.price) & !duplicated(woo$SKU) & !(woo$SKU == "")) 
+sales <- woo %>% filter(!is.na(woo$Sale.price)) 
 rownames(sales) <- sales$SKU
 
 # ------- match Square stock to xoro ------
@@ -13,18 +13,10 @@ rownames(sales) <- sales$SKU
 xoro <- read.csv(paste0("../xoro/", list.files(path = "../xoro/", pattern = paste0("Item Inventory Snapshot_", format(Sys.Date(), "%m%d%Y"), ".csv"))), as.is = T) %>% filter(Store == "Warehouse - JJ")
 rownames(xoro) <- xoro$Item.
 
-square <- data.frame(ItemName = woo1$SKU, SKU = woo1$SKU, Description = "", Category = xoro[woo1$SKU, "Item.Category"], Price = as.integer(woo1$Regular.price*0.8*100)/100, PriceRichmond = woo1$Regular.price, PriceSurrey = woo1$Regular.price, NewQuantityRichmond = xoro[woo1$SKU, "ATS"], NewQuantitySurrey = xoro[woo1$SKU, "ATS"], Token = "", VariationName = "Regular", EnabledRichmond = "Y", CurrentQuantityRichmond = "", CurrentQuantitySurrey = "", StockAlertEnabledRichmond = "N", StockAlertCountRichmond = 0, EnabledSurrey = "Y", StockAlertEnabledSurrey = "N", StockAlertCountSurrey = 0, TaxPST = woo1$Tax.class)
+square <- data.frame(ItemName = woo$SKU, SKU = woo$SKU, Description = "", Category = xoro[woo$SKU, "Item.Category"], Price = woo$Regular.price, PriceRichmond = woo$Regular.price, PriceSurrey = woo$Regular.price, NewQuantityRichmond = xoro[woo$SKU, "ATS"], NewQuantitySurrey = xoro[woo$SKU, "ATS"], Token = "", VariationName = "Regular", EnabledRichmond = "Y", CurrentQuantityRichmond = "", CurrentQuantitySurrey = "", StockAlertEnabledRichmond = "N", StockAlertCountRichmond = 0, EnabledSurrey = "Y", StockAlertEnabledSurrey = "N", StockAlertCountSurrey = 0, TaxPST = woo$Tax.class)
 rownames(square) <- square$SKU
 square$TaxPST <- gsub("parent", "N", gsub("full", "Y", square$TaxPST))
-square[grepl("WSS-CNL", square$SKU), "Price"] <- 39
-square[grepl("WSS-DNL", square$SKU), "Price"] <- 39
-square[grepl("WSS-UNC", square$SKU), "Price"] <- 39
-square[grepl("WSS-TRZ", square$SKU), "Price"] <- 39
-square[grepl("BRC", square$SKU), "Price"] <- 34
-square[grepl("LBS", square$SKU), "Price"] <- 27
-square[grepl("AWWJ", square$SKU), "Price"] <- 59
-square[grepl("ICP", square$SKU), "Price"] <- 69
+square[sales$SKU, "Price"] <- sales[sales$SKU, "Sale.price"]
 square$PriceRichmond <- square$Price
 square$PriceSurrey <- square$Price
-#square[sales$SKU, "Price"] <- sales[sales$SKU, "Sale.price"]
 write.table(square, file = paste0("../Square/square-upload-", Sys.Date(), ".csv"), sep = ",", row.names = F, col.names = c(colnames(square)[1:ncol(square)-1], "Tax-PST(7%)"), na = "")
