2efd37b99a2d37412ed6c7a50850c0ac13c836b6
Luolan Li <glorialuolanli@gmail.com>
2024-12-12 17:04:08 -0800

    Corrections.

--- routine.R
+++ routine.R
@@ -439,9 +439,9 @@ write_excel_csv(PO_NS, file = paste0(gsub("(.*\\/).*", "\\1", f), "NS_PO_", ID, 
 ID <- "CEFA6"; season <- "25S"; ReceiveDate <- "4/20/2025"
 netsuite_item <- read.csv(rownames(file.info(list.files(path = "../NetSuite/", pattern = "Items_All_", full.names = TRUE)) %>% filter(mtime == max(mtime))), as.is = T)
 PO <- openxlsx::read.xlsx(rownames(file.info(list.files(path = "../PO/order/CEFA/", pattern = "Order.*.xlsx", full.names = TRUE)) %>% filter(mtime == max(mtime))), sheet = 1, startRow = 2) %>% 
-  filter(grepl("-.*-", SKU.Number)) %>% mutate(Quantity = ifelse(is.na(Quantity), 0, Quantity), cat = gsub("-.*", "", SKU.Number))
+  filter(grepl("-.*-", SKU.Number), !is.na(Quantity), Quantity > 0) %>% mutate(cat = gsub("-.*", "", SKU.Number))
 CAT <- paste((PO %>% distinct(cat))$cat, collapse = " ")
-new_SKU <- PO %>% filter(!(SKU.Number %in% netsuite_item$Name), Quantity > 0) %>% select(SKU.Number, Quantity)
+new_SKU <- PO %>% filter(!(SKU.Number %in% netsuite_item$Name)) %>% select(SKU.Number, Quantity)
 if(nrow(new_SKU)){write.csv(new_SKU, file = paste0("../PO/order/CEFA/", "new_SKU_", ID, ".csv"), row.names = F, quote = F, na = "")}
 PO_NS <- PO %>% mutate(PO.TYPE = "Daycare Uniforms", CATEGORY = CAT, SEASON = season, REF.NO = ID, WAREHOUSE = "WH-SURREY", VENDOR = "China", CURRENCY = "CAD", ORDER.DATE = format(Sys.Date(), "%m/%d/%Y"), ORDER.PLACED.BY = "Gloria Li", APPROVAL.STATUS = "APPROVED", DUE.DATE = ReceiveDate, MEMO = paste0("CEFA order for ", season), ITEM = SKU.Number,  QUANTITY = Quantity, Tax.Code = "CA-Zero", External.ID = REF.NO) %>% 
   filter(Quantity > 0) %>% select(PO.TYPE:External.ID) %>% rename_with(~ gsub("\\.", " ", .))
